50c049dd08117a75e8b3044196b530ccec0ae41b
ClaudioArielNunez <claudioariel_n@hotmail.com>
2023-04-07 11:26:19 -0300

    Primer commit, armado de clases iniciales, conexion a DB

---
+++ PokemonApp/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace PokemonApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvPokemon = new System.Windows.Forms.DataGridView();
+            this.pbxPokemon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPokemon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPokemon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPokemon
+            // 
+            this.dgvPokemon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPokemon.Location = new System.Drawing.Point(40, 86);
+            this.dgvPokemon.Name = "dgvPokemon";
+            this.dgvPokemon.RowHeadersWidth = 62;
+            this.dgvPokemon.RowTemplate.Height = 28;
+            this.dgvPokemon.Size = new System.Drawing.Size(589, 262);
+            this.dgvPokemon.TabIndex = 0;
+            // 
+            // pbxPokemon
+            // 
+            this.pbxPokemon.Location = new System.Drawing.Point(660, 86);
+            this.pbxPokemon.Name = "pbxPokemon";
+            this.pbxPokemon.Size = new System.Drawing.Size(267, 262);
+            this.pbxPokemon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxPokemon.TabIndex = 1;
+            this.pbxPokemon.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(957, 450);
+            this.Controls.Add(this.pbxPokemon);
+            this.Controls.Add(this.dgvPokemon);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPokemon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPokemon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPokemon;
+        private System.Windows.Forms.PictureBox pbxPokemon;
+    }
+}
+
